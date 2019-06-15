d9eeb76f5773c523ce8f8949079fbe21335ca201
Arbaz Mateen <arbazmateen@gmail.com>
2019-06-15 12:54:58 +0500

    changes

--- extensions/src/main/java/com/arbazmateen/extensions/ActivityExtensions.kt
+++ extensions/src/main/java/com/arbazmateen/extensions/ActivityExtensions.kt
@@ -9,7 +9,6 @@ import android.view.View
 import android.view.WindowManager
 import android.view.inputmethod.InputMethodManager
 import android.widget.Toast
-import androidx.core.content.ContextCompat
 import java.io.Serializable
 
 fun Activity.toastLong(text: String) {
